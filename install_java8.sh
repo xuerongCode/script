e208cdb65ec0f09b804ca83990e727021353c5b4
xuerongCode <xuerong.work@gmail.com>
2018-06-07 17:00:11 -0400

    fix bug in install_java.sh

---
+++ install_java8.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -y update
+sudo apt-get install -y oracle-java8-installer
+echo 'JAVA_HOME="/user/lib/jvm/java-8-oracle"' | sudo tee /etc/environment
+#remember do source /etc/environment
