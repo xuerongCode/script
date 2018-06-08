f01d898e00b527a426f4b0bd220d25d899158da4
xuerongCode <xuerong.work@gmail.com>
2018-06-08 13:31:14 -0400

    fix java8 script

--- install_java8.sh
+++ install_java8.sh
@@ -2,5 +2,5 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get -y update
 sudo apt-get install -y oracle-java8-installer
-echo 'JAVA_HOME="/user/lib/jvm/java-8-oracle"' | sudo tee /etc/environment
-#remember do source /etc/environment
+echo '#JAVA_HOME path set up' | sudo tee ~/.profile
+echo 'JAVA_HOME="/user/lib/jvm/java-8-oracle"' | sudo tee ~/.profile 
